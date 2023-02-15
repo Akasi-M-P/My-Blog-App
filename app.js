2f8c4464cc662f452a761e105b6593aaafbb35c3
Mba96 <mbajameel90@gmail.com>
2023-02-15 23:55:27 +0000

    render composed post on home page, and render each post on the post page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash');
 
 const homeContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,9 +16,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const myPosts = [];
+
 //Root route
 app.get("/", function(req,res) {
-res.render("home",{firstContent: homeContent});
+res.render("home",{firstContent: homeContent,
+                  myPosts: myPosts});
+
 
 
 });
@@ -31,6 +36,7 @@ app.get("/about", function(req, res) {
 
 // Contact Page route
 app.get("/contact", function(req, res) {
+
   res.render("contact",{firstContent:contactContent});
 });
 
@@ -41,17 +47,44 @@ app.get("/compose", function(req, res) {
 
 
 app.post("/compose", function(req, res) {
-  // let newPost = req.body.newArticle;
 
-  // console.log(newPost);
- const newArticle = {
-  title: req.body.postTitle,
-  body: req.body.postBody
- };
- console.log(newArticle);
+let newArticle = {
+ title : req.body.postTitle,
+ body: req.body.postBody
+}
+
+myPosts.push(newArticle);
+
+res.redirect("/");
+ 
+
 
 });
 
+
+app.get("/myPosts/:postName", function(req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+  
+
+  myPosts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+    
+ 
+
+    if (storedTitle === requestedTitle) {
+     res.render("post", {
+      title: post.title,
+      body: post.body
+     });
+    };
+  });
+});
+
+
+// app.get("/post", function(req, res) {
+//   res.render("post",{myPosts: myPosts});
+
+// });
 
 
 
